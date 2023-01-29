3ecee0904bb6d64f80e12b2feb073f43642acf1e
Umut Developer <umutdevcontact@gmail.com>
2023-01-29 18:11:35 +0300

    Initial pages commit

--- example/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}', './node_modules/@uc-react-ui/multiselect/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
